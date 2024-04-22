1878ed85bc601352610b884722a5a1f32b65919f
Bill Jellesma <william.jellesma@gmail.com>
2024-04-22 00:32:07 -0400

    add get post and put endpoints

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Validations;
 
 namespace api.Controllers
 {
@@ -23,7 +26,9 @@ namespace api.Controllers
             // LINQ queries are deferred by nature meaning that the results are not returned until they're actually needed by the application
             // using ToList forces them to be retrieved at this specific point when we're calling this
             // so essentially we're bypassing the defered and saying that we want this right now
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList()
+                // select is like map in this case
+                .Select(s => s.ToStockDto());
 
             return Ok(stocks);
         }
@@ -38,7 +43,40 @@ namespace api.Controllers
                 return NotFound();
             }
 
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        // fromBody is an annotation saying that whatever is passed in the body of the request will exist in that param
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto){
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            // add will just add the data to the buffer on the entity framework while save changes will actually run the sql
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            // This created at action is going to perform getbyid, with the param provided in the second arg, and tostockdto is going to be the final action
+            return CreatedAtAction(nameof(GetById), new {id = stockModel.Id}, stockModel.ToStockDto());
+
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto){
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if(stockModel == null){
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
         }
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1edcb8c7f71e9765798fec1416f6176dde11b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
